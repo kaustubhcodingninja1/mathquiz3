8d30b1a07d6998e61e03cc43e6ec9c470e5fbc96
kaustubhcodingninja1 <92898332+kaustubhcodingninja1@users.noreply.github.com>
2021-12-21 19:24:14 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+<script src="quiz_game.js"></script>
+</body>
+</html>
+
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
